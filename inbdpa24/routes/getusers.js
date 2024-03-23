9ee89e9f61b2136f4abc8e488a4c14d2aba3aaff
RamenXk <158761011+RamenXk@users.noreply.github.com>
2024-03-23 18:00:13 +0000

    profiles made

--- inbdpa24/routes/getusers.js
+++ inbdpa24/routes/getusers.js
@@ -3,10 +3,11 @@ var router = express.Router();
 // Normal include statements
 
 const myGetRestCall=require("../middleware/RestAPIGet");
+const myIncrementRestCall = require("../middleware/RestAPIIncrement");
 //including middleware
 
 router.get('/', function(req,res,next) {
-    const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users';
+    const url = 'https://inbdpa.api.hscc.bdpa.org/v1/user'
     const token = process.env.BEARER_TOKEN;
     //console.log(url); //Debug
 
@@ -28,6 +29,66 @@ router.get('/', function(req,res,next) {
         }
     }) // data then component
     .catch(error => console.error(error));
-}); // close router.get route
+}); // close router.get general route
+
+// GET route for a specific user (based on username)
+router.get('/:username', function(req,res,next) {
+    const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/' + req.params.username;
+    const token = process.env.BEARER_TOKEN;
+
+    //console.log(url); //Debugging code to test url; I'll comment it out once it works
+
+    // Pass url and token into RestAPIGet and pull information from response
+    myGetRestCall.getWithBearerToken(url, token)
+    .then(data => {
+        console.log('REST CALL',data);
+        if (data.success){
+            // SUBJECT TO CHANGE
+            var username=data.user.username;
+            var usertype=data.user.type;
+            var views=data.user.views;
+            var aboutsection=data.user.sections.about;
+            var educationsection=data.user.sections.education;
+            var experiencesection=data.user.sections.experience;
+            var skillssection=data.user.sections.skills;
+            var volunteeringsection=data.user.sections.volunteering;
+            var userid=data.user.user_id;
+
+            const idurl='https://inbdpa.api.hscc.bdpa.org/v1/users/' + userid;
+
+            console.log(idurl);
+
+            myIncrementRestCall.incrementWithBearerToken(idurl, token);
+            res.render('userprofile',
+            {title: 'User profile',
+            username: username,
+            type: usertype,
+            views: views+1,
+            about: aboutsection,
+            education: educationsection,
+            experience: experiencesection,
+            skills: skillssection,
+            volunteering: volunteeringsection
+            }); //closes res.render statement
+
+
+
+        } // closes if statement
+        else{
+            res.render('error', {title: 'User call failed', message: data.error});
+        }
+
+
+    }) // data then component
+    .catch(error => console.error(error));
+
+    //res.render('index', {title: 'debugging user profile page'});
+
+
+}); // close router.get username route
+
+
+
+
 
 module.exports=router;
